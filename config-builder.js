66d2a5c8a730226a933983396f07e287aa928148
Arpan Tolat <Arpan Tolat>
2016-08-03 03:07:42 -0500

    testing

--- config-builder.js
+++ config-builder.js
@@ -1,6 +1,20 @@
+//INITIAL CONFIGS...
+var PORT =process.env.PORT || 3000;
+var express = require('express');
 var prompt = require('prompt');
 var color = require('colors/safe');
 var fs = require('fs');
+var knox = require('knox');
+var app = express();
+
+//Create the S3 client
+var client = knox.createClient({
+    key: 'AKIAJTSXS6U2YVRJ3IAQ'
+  , secret: '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'
+  , bucket: 'help-config-bucket'
+});
+
+//DEFINING configuration schema
 var configSchema = {
 	properties: {
 		img_urls: {
@@ -24,6 +38,8 @@ var configSchema = {
 		}
 	}
 }
+
+//Start CL prompts.
 prompt.start();
 prompt.message = '>>';
 
@@ -31,17 +47,26 @@ prompt.get(configSchema,function(error,result){
 	console.log(result);
 
     var str = JSON.stringify(result);
-    fs.writeFile("./temp", str, function(err) {
+    fs.writeFile("./temp.txt", str, function(err) {
     if(err) {
         return console.log(err);
     }
 
     console.log("The file was saved!");
+    client.putFile('./temp.txt', {'Content-Type': 'text/plain'}, function(err, result) {
+    if (200 == result.statusCode) { console.log('Uploaded to mazon S3'); }
+    else { console.log('Failed to upload file to Amazon S3'); }
+});
 });
 
 
 
 }); 
  
+app.get('/',function(req, res){
+    res.send('Root');
+});
 
-
+app.listen(PORT, function(){
+    console.log('Express server started on port:: '+PORT);
+});
