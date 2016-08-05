2470095b9db69945a8ca0a9b40e0c6d135f012ca
atolat <atolat@uci.edu>
2016-08-05 01:32:55 -0500

    Karma will get you

--- config-builder.js
+++ config-builder.js
@@ -3,7 +3,7 @@ var PORT =process.env.PORT || 3000;
 var express = require('express');
 var knox = require('knox');
 var app = express();
-var routes = require('./routes/server')(app);
+var routes = require('./routes/scratchpad-server')(app);
 
 //CORS Configuration
 allowCrossDomain = function (req, res, next) {
@@ -19,64 +19,6 @@ allowCrossDomain = function (req, res, next) {
 
 app.use(allowCrossDomain);
 
-
-
-
-//Create the S3 client
-// var client = knox.createClient({
-//     key: 'AKIAJTSXS6U2YVRJ3IAQ'
-//   , secret: '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'
-//   , bucket: 'help-config-bucket'
-// });
-
-//DEFINING configuration schema
-// var configSchema = {
-// 	properties: {
-// 		img_urls: {
-// 			properties: {
-// 				shoutout:{
-// 					description: color.blue('Enter the url for the shoutout image::')
-// 				},
-// 				tooltip1:{
-// 					description: color.magenta('Enter the url of the first tooltip image::')
-// 				},
-// 				tooltip2:{
-// 					description: color.magenta('Enter the url of the second tooltip image::')
-// 				},
-// 				tooltip3: {
-// 					description: color.magenta('Enter the url of the third tooltip image::')
-// 				}
-// 			}
-// 		},
-// 		period: {
-// 			description: color.green('Enter the period for shoutouts--Monthly/Weekly/Quarterly/Daily::')
-// 		}
-// 	}
-// }
-
-//Start CL prompts.
-// prompt.start();
-// prompt.message = '>>';
-
-// prompt.get(configSchema,function(error,result){
-// 	console.log(result);
-
-//     var string = JSON.stringify(result);
-
-//  var req = client.put('/test/obj.json', {
-//     'Content-Length': Buffer.byteLength(string)
-//   , 'Content-Type': 'application/json'
-//   , 'x-amz-acl': 'public-read'
-// });
-// req.on('response', function(res){
-//   if (200 == res.statusCode) {
-//     console.log('saved to %s', req.url);
-//   }
-// });
-// req.end(string);
-
-// }); 
- 
 
 app.listen(PORT, function(){
     console.log('Express server started on port:: '+PORT);
---
+++ routes/scratchpad-server.js
@@ -0,0 +1,102 @@
+module.exports = function (app) {
+
+    var _ = require('underscore');
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    var knox = require('knox');
+
+    //Create the S3 client
+    var client = knox.createClient({
+        key: 'AKIAJTSXS6U2YVRJ3IAQ'
+        , secret: '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'
+        , bucket: 'help-config-bucket'
+    });
+
+
+    client.get('/test/obj.json').on('response', function(res){
+  console.log(res.statusCode);
+  console.log(res.headers);
+  res.setEncoding('utf8');
+  res.on('data', function(chunk){
+    console.log(chunk);
+  });
+}).end();
+
+//     var configSchema = {
+//         tooltips : {
+//             tooltip1_url : '',
+//             tooltip2_url : '',
+//             tooltip3_url : ''
+
+//         },
+//         resources : {
+//             video_resource_url : '',
+//             pdf_resource_url : ''
+//         },
+//         shoutout:{
+//             shoutout_url : '',
+//             period : ''
+//         }
+//     };
+
+
+//     //API ROOT
+//     app.get('/', function (req, res) {
+//         res.sendfile('./config-form.html');
+//     });
+
+//     app.use(bodyParser.urlencoded({
+//         extended: true
+//     }));
+
+// /**bodyParser.json(options)
+//  * Parses the text as JSON and exposes the resulting object on req.body.
+//  */
+// app.use(bodyParser.json());
+
+// app.post("/buildjson", function (req, res) {
+    
+//     console.log(req.body.flow.tooltip1_url);
+//     console.log(req.body.flow.tooltip2_url);
+//     console.log(req.body.flow.tooltip3_url);
+//     console.log(req.body.flow.pdf_resource_url);
+//     console.log(req.body.flow.video_resource_url);
+//     console.log(req.body.flow.shoutout_url);
+//     console.log(req.body.flow.period);
+
+//     configSchema.tooltips.tooltip1_url = req.body.flow.tooltip1_url;
+//     configSchema.tooltips.tooltip2_url = req.body.flow.tooltip2_url;
+//     configSchema.tooltips.tooltip3_url = req.body.flow.tooltip3_url;
+//     configSchema.resources.pdf_resource_url = req.body.flow.pdf_resource_url;
+//     configSchema.resources.video_resource_url = req.body.flow.video_resource_url;
+//     configSchema.shoutout.shoutout_url = req.body.flow.shoutout_url;
+//     configSchema.shoutout.period = req.body.flow.period;
+
+//     console.log(configSchema);
+
+//     var string = JSON.stringify(configSchema);
+
+//     var req = client.put('/test/obj.json', {
+//         'Content-Length': Buffer.byteLength(string)
+//         , 'Content-Type': 'application/json'
+//         , 'x-amz-acl': 'public-read'
+//     });
+    
+//     req.on('response', function(res){
+        
+//         if (200 == res.statusCode) {
+//             console.log('saved to %s', req.url);
+//         }
+//     });
+    
+//     req.end(string);
+    
+
+    
+    
+//     res.send('Cool!');
+  
+// });
+
+    
+}
