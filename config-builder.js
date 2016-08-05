c8f5ca4165e32b0f9cf3be1da443b70b8ba22457
atolat <atolat@uci.edu>
2016-08-05 08:52:59 -0500

    Global Refactoring!!

--- config-builder.js
+++ config-builder.js
@@ -117,7 +117,7 @@ app.use(allowCrossDomain);
 
 
 app.post('/buildform',function(req,res){
-	var data = JSON.stringify(req.body.bucket.data);
+	var data = JSON.parse(req.body.bucket);
 	console.log(data.tooltips);
 	res.send('success');
 });
