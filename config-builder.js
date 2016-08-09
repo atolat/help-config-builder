ded43e21cbfabe3387f911a67d998c7ba482da94
atolat <atolat@uci.edu>
2016-08-09 08:52:57 -0500

    bootstrapped

--- config-builder.js
+++ config-builder.js
@@ -72,6 +72,8 @@ app.post('/buildform',function(req,res){
 
     var htmlTail = '<div class="form-group"><label class="col-md-4 control-label" for="singlebutton"></label><div class="col-md-4"><button type = "submit" value = "submit" id="singlebutton" name="singlebutton" class="btn btn-primary">Update Flow</button></div></div></fieldset></form></body>';
 
+    
+    
     for(i=1;i<=tooltip_num;i++){
         tooltipFieldHtml = tooltipFieldHtml + '<div class="form-group"><label class="col-md-4 control-label" for="tooltips[tooltip'+i+'_url]">Tooltip '+i+' URL</label><div class="col-md-4"><input id="tooltips[tooltip'+i+'_url]" name="tooltips[tooltip'+i+'_url]" type="text" placeholder="" class="form-control input-md"></div></div>';
     }
