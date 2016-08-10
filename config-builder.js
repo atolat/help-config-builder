737f03fd991f242402c8a588151dc18b9ff97337
atolat <atolat@uci.edu>
2016-08-10 13:40:01 -0500

    Added tableify

--- config-builder.js
+++ config-builder.js
@@ -100,10 +100,10 @@ app.post('/buildform',function(req,res){
 
     for(i=1;i<=pdf_resource_num;i++){
         pdfFieldHtml = pdfFieldHtml + '<div class="form-group"><label class="col-md-4 control-label" for="pdf[pdf'+i+'_url]">PDF '+i+' URL</label><div class="col-md-4"><input id="pdf[pdf'+i+'_url]" name="pdf[pdf'+i+'_url]" type="text" placeholder="'+pdf_arr[i-1]+'" class="form-control input-md"></div></div>';
-        pdfBuff = pdfBuff + '<li class="list-group-item list-group-item-warn">'+pdf_arr[i-1]+'</li>';
+        pdfBuff = pdfBuff + '<li class="list-group-item list-group-item-warning">'+pdf_arr[i-1]+'</li>';
     }
 
-	res.send(htmlHead+tooltipFieldHtml+videoFieldHtml+pdfFieldHtml+hidden+htmlTail+'<div class="container"><h1>Current Configurations</h1><h2>Tooltips</h2><ul class="list-group">'+tooltipBuff+'</ul></div>'+'<div class="container"><h2>Videos</h2><ul class="list-group">'+videoBuff+'</ul></div>'+'<div class="container"><h2>PDFs</h2><ul class="list-group">'+pdfBuff+'</ul></div>');
+	res.send(htmlHead+'<div class="container"><h1>Current Configurations</h1><h2>Tooltips</h2><ul class="list-group">'+tooltipBuff+'</ul></div>'+'<div class="container"><h2>Videos</h2><ul class="list-group">'+videoBuff+'</ul></div>'+'<div class="container"><h2>PDFs</h2><ul class="list-group">'+pdfBuff+'</ul></div>'+tooltipFieldHtml+videoFieldHtml+pdfFieldHtml+hidden+htmlTail);
 });
 
 app.post("/updatejson", function (req, res) {
